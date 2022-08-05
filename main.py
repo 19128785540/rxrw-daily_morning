79fbd12be4db3ac731744979943233771171a739
Jens Lee <rxrw@me.com>
2022-08-06 03:28:00 +0800

    fix origin api error

--- main.py
+++ main.py
@@ -35,10 +35,10 @@ def get_birthday():
   return (next - today).days
 
 def get_words():
-  words = requests.get("https://api.vvhan.com/api/love")
+  words = requests.get("https://api.shadiao.pro/chp")
   if words.status_code != 200:
     return get_words()
-  return words.text
+  return words.json()['data']['text']
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
