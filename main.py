d0ac6f9570e697779c312a5c249130fd2f6efa9c
Jian Li <jian.li@rightcapital.com>
2022-08-25 17:38:21 +0800

    优化报错提示，优化代码结构，优化变量读取

--- main.py
+++ main.py
@@ -1,57 +1,136 @@
 from datetime import date, datetime, timedelta
 import math
-from wechatpy import WeChatClient
-from wechatpy.client.api import WeChatMessage, WeChatTemplate
+from wechatpy import WeChatClient, WeChatClientException
+from wechatpy.client.api import WeChatMessage
 import requests
 import os
 import random
 
 today = datetime.now() + timedelta(hours=8)
-start_date = os.environ['START_DATE']
-city = os.environ['CITY']
-birthday = os.environ['BIRTHDAY']
+start_date = os.getenv('START_DATE')
+city = os.getenv('CITY')
+birthday = os.getenv('BIRTHDAY')
 
-app_id = os.environ["APP_ID"]
-app_secret = os.environ["APP_SECRET"]
+app_id = os.getenv('APP_ID')
+app_secret = os.getenv('APP_SECRET')
 
-user_ids = os.environ["USER_ID"].split("\n")
-template_id = os.environ["TEMPLATE_ID"]
+user_ids = os.getenv('USER_ID', '').split("\n")
+template_id = os.getenv('TEMPLATE_ID')
 
+if app_id is None or app_secret is None:
+  print('请设置 APP_ID 和 APP_SECRET')
+  exit(422)
 
+if not user_ids:
+  print('请设置 USER_ID，若存在多个 ID 用空格分开')
+  exit(422)
+
+if template_id is None:
+  print('请设置 TEMPLATE_ID')
+  exit(422)
+
+# weather 直接返回对象，在使用的地方用字段进行调用。
 def get_weather():
+  if city is None:
+    print('请设置城市')
+    return None
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
+  if res is None:
+    return None
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp']), math.floor(weather['high']), math.floor(weather['low'])
+  return weather
 
-def get_count():
+# 纪念日正数
+def get_memorial_days_count():
+  if start_date is None:
+    print('没有设置 START_DATE')
+    return 0
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
   return delta.days
 
-def get_birthday():
+# 生日倒计时
+def get_birthday_left():
+  if birthday is None:
+    print('没有设置 BIRTHDAY')
+    return 0
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
 
+# 彩虹屁 接口不稳定，所以失败的话会重新调用，直到成功
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
   if words.status_code != 200:
     return get_words()
   return words.json()['data']['text']
 
+def format_temperature(temperature):
+  return math.floor(temperature)
+
+# 随机颜色
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
-
-client = WeChatClient(app_id, app_secret)
+try:
+  client = WeChatClient(app_id, app_secret)
+except WeChatClientException as e:
+  print('微信获取 token 失败，请检查 APP_ID 和 APP_SECRET，或当日调用量是否已达到微信限制。')
+  exit(502)
 
 wm = WeChatMessage(client)
-wea, temperature, highest, lowest = get_weather()
-data = {"city":{"value":city,"color":get_random_color()},"date":{"value":today.strftime('%Y年%m月%d日'),"color":get_random_color()},"weather":{"value":wea,"color":get_random_color()},"temperature":{"value":temperature,"color":get_random_color()},"love_days":{"value":get_count(),"color":get_random_color()},"birthday_left":{"value":get_birthday(),"color":get_random_color()},"words":{"value":get_words(),"color":get_random_color()},"highest": {"value":highest,"color":get_random_color()},"lowest":{"value":lowest, "color":get_random_color()}}
-count = 0
-for user_id in user_ids:
-  res = wm.send_template(user_id, template_id, data)
-  count+=1
+weather = get_weather()
+if weather is None:
+  print('获取天气失败')
+  exit(422)
+data = {
+  "city": {
+    "value": city,
+    "color": get_random_color()
+  },
+  "date": {
+    "value": today.strftime('%Y年%m月%d日'),
+    "color": get_random_color()
+  },
+  "weather": {
+    "value": weather['weather'],
+    "color": get_random_color()
+  },
+  "temperature": {
+    "value": math.floor(weather['temp']),
+    "color": get_random_color()
+  },
+  "highest": {
+    "value": math.floor(weather['high']),
+    "color": get_random_color()
+  },
+  "lowest": {
+    "value": math.floor(weather['low']),
+    "color": get_random_color()
+  },
+  "love_days": {
+    "value": get_memorial_days_count(),
+    "color": get_random_color()
+  },
+  "birthday_left": {
+    "value": get_birthday_left(),
+    "color": get_random_color()
+  },
+  "words": {
+    "value": get_words(),
+    "color": get_random_color()
+  },
+}
 
-print("发送了" + str(count) + "条消息")
+if __name__ == '__main__':
+  count = 0
+  try:
+    for user_id in user_ids:
+      res = wm.send_template(user_id, template_id, data)
+      count+=1
+  except WeChatClientException as e:
+    print('微信端返回错误：%s。错误代码：%d' % (e.errmsg, e.errcode))
+    exit(502)
+
+  print("发送了" + str(count) + "条消息")
